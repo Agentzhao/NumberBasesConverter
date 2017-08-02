2e7501b301f1dd0197bb3354f9fe3050b0bb91c8
Agentzhao <tan.hongzhao@dhs.sg>
2017-08-02 10:35:43 +0800

    Update and rename bindec.py to binarytodecimal.py

--- binarytodecimal.py
+++ binarytodecimal.py
@@ -1,8 +1,7 @@
 def binarytodecimal(num):
     b = list(str(num)).reverse()
     dec = 0
-    base = 2
     for i in range(0,len(b)):
-        num = int(b[i])*(base**i)
+        num = int(b[i])*(2**i)
         dec += num
     return dec
