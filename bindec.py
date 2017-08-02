a79cf89ed7f7eb63e917832eebc188c4fb6c19a1
Agentzhao <tan.hongzhao@dhs.sg>
2017-08-02 10:27:02 +0800

    Update bindec.py

--- bindec.py
+++ bindec.py
@@ -1,12 +1,8 @@
-def binary_decimal(num):
-    b = list(str(num))
-    b.reverse()
+def binarytodecimal(num):
+    b = list(str(num)).reverse()
     dec = 0
     base = 2
     for i in range(0,len(b)):
         num = int(b[i])*(base**i)
         dec += num
     return dec
-
-# main
-print(binary_decimal(10110))
