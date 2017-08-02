3b8ef20578133351dd589dec673eb3b6221e983c
Agentzhao <tan.hongzhao@dhs.sg>
2017-08-02 10:29:47 +0800

    Update dechex.py

--- dechex.py
+++ dechex.py
@@ -1,7 +1,20 @@
-bases = {'1':'1','2':'2','3':'3','4':'4','5':'5','6':'6','7':'7','8':'8',\
-         '9':'9','10':'A','11':'B','12':'C','13':'D','14':'E','15':'F'}
+bases = {'1':'1',
+         '2':'2',
+         '3':'3',
+         '4':'4',
+         '5':'5',
+         '6':'6',
+         '7':'7',
+         '8':'8',
+         '9':'9',
+         '10':'A',
+         '11':'B',
+         '12':'C',
+         '13':'D',
+         '14':'E',
+         '15':'F'}
 
-def decimal_hexa(dec):
+def decimaltohexa(dec):
     num = int(dec)
     hexa = []
     while (num > 0):
@@ -10,7 +23,3 @@ def decimal_hexa(dec):
         num = num // 16
     hexa.reverse()
     return(''.join(hexa))
-
-
-# main
-print(decimal_hexa(95))
